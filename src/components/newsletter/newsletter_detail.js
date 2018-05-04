d6b941ebac22868adeec6f43233ae0ddee0bbfa2
Gaven Neufeld <gneuf91@gmail.com>
2018-05-04 14:55:39 -0600

    added newsletter detail component

---
+++ src/components/newsletter/newsletter_detail.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewletterDetail extends Component {
+    render() {
+       return (
+            <div>
+                newsletter detail
+            </div>
+        )
+    }
+}
+
+export default NewletterDetail;
--- src/reducers/newsletterReducer.js
+++ src/reducers/newsletterReducer.js
@@ -3,14 +3,14 @@ import { FETCH_NEWSLETTER_ARCHIVE } from "../actions/types";
 export default function(state = {}, action) {
     switch(action.type) {
         case FETCH_NEWSLETTER_ARCHIVE:
-        const latestItem = action.payload[0];
-        const archive = action.payload;
-        archive.shift()
-            return { 
-                ...state,
-                archive,
-                latestItem 
-             }
-        default: return state;
+            const latestItem = action.payload[0];
+            const archive = action.payload;
+            archive.shift()
+                return { 
+                    ...state,
+                    archive,
+                    latestItem 
+                }
+            default: return state;
     }
 }
