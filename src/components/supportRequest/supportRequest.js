28ee9cc039bf9e5f2b872d3abdf0b29894c221cc
Gaven Neufeld <gneuf91@gmail.com>
2018-05-07 16:37:59 -0600

    added supportRequest component

---
+++ src/components/supportRequest/supportRequest.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SupportRequest extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
+export default SupportRequest;
