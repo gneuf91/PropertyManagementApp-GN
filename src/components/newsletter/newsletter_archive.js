2381e29ca7b97c0c16813c8200678435a5c2c8b4
Gaven Neufeld <gneuf91@gmail.com>
2018-05-11 16:14:58 -0600

    styled newsletterarchive component list

--- src/components/newsletter/newsletter_archive.js
+++ src/components/newsletter/newsletter_archive.js
@@ -12,9 +12,11 @@ class NewsletterArchive extends Component {
     renderNewsletterArchiveItem = function(item, index) {
         return (
             <li key={index} className="archive-item">
-                <div className="archive-item__title">
-                    <Link to={`/newsletter/detail/${item._id}`}>{item.title}</Link>
-                </div>
+                <Link className="archive-link-wrapper" to={`/newsletter/detail/${item._id}`}>
+                    <div className="archive-item__title">
+                        {item.title}
+                    </div>
+                </Link>
                 <div className="archive-item__date">09/15/2030</div>
             </li>
         )
@@ -27,10 +29,8 @@ class NewsletterArchive extends Component {
             })
         }
     }
-    
-   
+
     render() {
-        
         return (
             <div className="newsletter-archive">
                 <div className="archive__title">Archive</div>
